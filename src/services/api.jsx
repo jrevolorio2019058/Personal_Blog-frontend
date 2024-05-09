7a491409198f76539d6ca146ae6f8074eccbe083
Informatica <informatica@alumno.local>
2024-05-09 11:15:10 -0600

    Creating variable of register in API.jsx

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -48,3 +48,20 @@ export const login = async(data) => {
     }
 
 }
+
+export const register = async(data) => {
+    
+    try {
+        
+        return await apiClient.post('auth/register', data)
+
+    } catch (e) {
+        
+        return {
+            error: true,
+            e
+        }
+
+    }
+
+}
