97f412964f616571e08fb5f246567acc1ffe0892
jrevolorio2019058 <jrevolorio-2019058@kinal.edu.gt>
2024-05-12 23:43:25 -0600

    Post CSS created

--- src/componets/post/Post.jsx
+++ src/componets/post/Post.jsx
@@ -41,43 +41,73 @@ export const Post = () => {
 
     return (
 
-        <div>
-            {isLoading ? (
-                <h5>Loading... Wait a Minute</h5>
-            ):(
-                <div>
-                    {Array.isArray(post) && post.length > 0 ?
-                    (
-                        post.map(post =>(
+        <div className="full-page">
+            <div className="content">
+                {isLoading ? (
+                    <p>Loading... Wait a Minute</p>
+                ):(
+                    <div className="dirrection">
+                        {Array.isArray(post) && post.length > 0 ?
+                        (
+                            post.map(post =>(
 
-                            <div key={post._id}>
+                                <div key={post._id}>
 
-                                <div>
+                                    <div className="cards">
 
-                                    <h2>{post.postName}</h2>
-                                    <h2>{post.postDescription}</h2>
-                                    <h2>{post.postImage}</h2>
-                                    <h2>{post.proyectLink}</h2>
-                                    <h2>{post.nameCreator}</h2>
-                                    <h2>{post.nameComments}</h2>
+                                        <h1 className="tittle">{post.postName}</h1>
+
+                                        <h2 className="creator">Creator: {post.nameCreator}</h2>
+
+                                        <div className="image">
+                                            <img src={post.postImage}/>
+                                        </div>
+
+                                        <h2 className="description">- {post.postDescription}</h2>
+
+                                        <div className="link-container">
+
+                                            {Array.isArray(post.proyectLink) && post.proyectLink.length > 1? (
+                                                post.proyectLink.map((link, index) => (
+                                                    <a key={index} href={link} target="_blank" rel="noopener noreferrer">
+                                                        <img className="link" src='https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png' />
+                                                    </a>
+                                                ))
+                                            ) : (
+                                                
+                                                <div className="link">
+                                                    <img src='https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png' alt={post.proyectLink}></img>
+                                                </div>
+                                            )}
+
+                                        </div>
+
+                                    </div>
 
                                 </div>
 
-                            </div>
+                            ))
+                        )    : (
 
-                        ))
-                    )    : (
+                            <h5>NOT FOUND DATA, Sorry</h5>
 
-                        <h5>NOT FOUND DATA, Sorry</h5>
-
-                    )
+                        )
+                    }
+                    </div>
+                )
                 }
-                </div>
-            )
-            }
+            </div>
         </div>
 
     )
 
 
 }
+
+/*{Array.isArray(post.nameComments) && post.nameComments.length > 1? (
+    post.nameComments.map((link, index) => (
+        <div className="comments" key={index}>{link}</div>
+    ))
+) : (
+    <div className="comments">{post.nameComments}</div>
+)}*/
--- src/shared/hooks/useLogin.jsx
+++ src/shared/hooks/useLogin.jsx
@@ -1,22 +1,17 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { login as loginRequest } from '../../services'
 import toast from "react-hot-toast";
 
 export const useLogin = () => {
-    const [isLoading, setIsLoading] = useState(false)
 
     const navigate = useNavigate()
 
     const login = async(usernameOrEmail, password) =>{
-        setIsLoading(true)
 
         const response = await loginRequest({
             usernameOrEmail,
             password
         })
-        console.log(response)
-        setIsLoading(false)
 
         if(response.error){
             console.log(response.error)
@@ -30,7 +25,6 @@ export const useLogin = () => {
         navigate('/')
     }
     return{
-        login,
-        isLoading
+        login
     }
 }
