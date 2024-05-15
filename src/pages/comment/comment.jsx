37e09ea0ab132d59cb769efaae932d0d33d541eb
jrevolorio2019058 <jrevolorio-2019058@kinal.edu.gt>
2024-05-14 23:48:49 -0600

    Final details

--- src/pages/comment/comment.jsx
+++ src/pages/comment/comment.jsx
@@ -1,12 +1,15 @@
 import { AddComment } from '../../componets/comment/AddComment';
 
+import { Navbar } from "../../componets/navbar/Navbar";
+
 import './comment.css';
 
 export const CommentPage = () => {
 
   return (
     <div>
-        <AddComment/>
+      <Navbar/>
+      <AddComment/>
     </div>
   );
 };
